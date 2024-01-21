0b94b366ded97fcce94f7104ef0d9883e40cc16a
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-21 21:15:06 +0100

    feature fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,9 +6,9 @@ function Banner() {
   const { filters, SetFilters } = useGlobal();
   return (
     <div className="absolute top-20  flex items-center justify-center w-screen ">
-      <section className="w-[80%] py-4 flex items-center justify-start px-4 z-10 shadow-md bg-white h-auto rounded-[4px] ">
+      <section className="w-[80%] py-4 flex items-center justify-start px-4 z-10 shadow-md bg-white gap-2 h-auto rounded-[4px] ">
         {filters.map((id) => {
-          return <Ban prop={prop} />;
+          return <Ban prop={id} />;
         })}
       </section>
       <p className="text-green-600">clear</p>
