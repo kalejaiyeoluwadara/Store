dd89ac9400a4fc5c02b1d5f8a746c4dbf8e17ad1
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-21 20:53:29 +0100

    changes

---
+++ src/components/Ban.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Ban() {
+  return (
+    <div className="px-0 py-0 flex bg-gray-200 text-green-600 font-[600] rounded-[3px] ">
+      <p className="px-2 py-1">HTML</p>
+      <p className="text-black w-[20px] h-full bg-green-500 ">x</p>
+    </div>
+  );
+}
+
+export default Ban;
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Ban from "./Ban";
 
 function Banner() {
   return (
     <div className="absolute top-20  flex items-center justify-center w-screen ">
-      <section className="w-[80%] z-10 shadow-md bg-white h-[9vh] rounded-[4px] "></section>
+      <section className="w-[80%] flex items-center justify-start px-4 z-10 shadow-md bg-white h-[9vh] rounded-[4px] ">
+        <Ban />
+      </section>
     </div>
   );
 }
