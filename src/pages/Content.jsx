c94e6f5096fd94ab25e5a57dd84b621fd1c0384e
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-01-22 22:48:16 +0100

    done

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -5,9 +5,17 @@ import { useGlobal } from "../context";
 
 function Content() {
   const { filters } = useGlobal();
+  const filteredData = data.filter((item) => {
+    if (filters.length === 0) {
+      return true; // If no filters are selected, include all items
+    }
+
+    // Check if any language in filters array is present in the item's languages
+    return filters.some((filter) => item.languages.includes(filter));
+  });
   return (
     <div className="p-10 flex mt-10 items-center justify-center flex-col sm:gap-5 gap-12 ">
-      {data.map((item) => {
+      {filteredData.map((item) => {
         const {
           id,
           company,
@@ -24,25 +32,19 @@ function Content() {
         } = item;
         return (
           <>
-            {filters.length <= 0 ? (
-              <Item
-                company={company}
-                logo={logo}
-                position={position}
-                postedAt={postedAt}
-                contract={contract}
-                languages={languages}
-                location={location}
-                featured={featured}
-                isNew={isNew}
-                level={level}
-                tools={tools}
-              />
-            ) : (
-              filters.includes((languages) => {
-                console.log("yes");
-              })
-            )}
+            <Item
+              company={company}
+              logo={logo}
+              position={position}
+              postedAt={postedAt}
+              contract={contract}
+              languages={languages}
+              location={location}
+              featured={featured}
+              isNew={isNew}
+              level={level}
+              tools={tools}
+            />
           </>
         );
       })}
